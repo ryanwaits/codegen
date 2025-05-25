b810bdf8ca900a3115d037bc642745fe97aea76b
Ryan Waits <ryan.waits@gmail.com>
2025-05-25 13:55:11 -0500

    feat: add React hooks and components for Stacks integration

---
+++ src/react/index.ts
@@ -0,0 +1,7 @@
+/**
+ * React integration for @stacks/codegen
+ */
+
+export { useStacksConfig } from "./hooks.js";
+export { createStacksConfig } from "./config.js";
+export type { StacksReactConfig } from "./types.js";
